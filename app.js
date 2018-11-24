711cd612466a531c1b98a270cbac1d822ce17276
Vidit Shah <vidit0210@gmail.com>
2018-11-24 20:23:54 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-//Author Vidit Shah
+const pug = require('pug');
+
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT ||3000;
+const PORT = process.env.PORT ||5000;
 
+app.set('view engine','pug');
 app.get('/',(req,res)=>{
-    res.send("Hello World");
+    res.render('data',{_title:'Block Chain Quiz 1',message:'Hello World'});
 });
-
 app.listen(PORT,()=>{console.log(`Listening on port ${PORT} `)});
